51f20f6a9becfec31cd28b9546bd3908d313892d
Jason Wang <jasowang@redhat.com>
2021-06-09 11:22:58 +0800

    virito-net: use "qps" instead of "queues" when possible
    Most of the time, "queues" really means queue pairs. So this patch
    switch to use "qps" to avoid confusion.
    
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- hw/net/vhost_net.c
+++ hw/net/vhost_net.c
@@ -333,7 +333,7 @@ int vhost_net_start(VirtIODevice *dev, NetClientState *ncs,
         if (i < data_qps) {
             peer = qemu_get_peer(ncs, i);
         } else { /* Control Virtqueue */
-            peer = qemu_get_peer(ncs, n->max_queues);
+            peer = qemu_get_peer(ncs, n->max_qps);
         }
 
         net = get_vhost_net(peer);
@@ -358,7 +358,7 @@ int vhost_net_start(VirtIODevice *dev, NetClientState *ncs,
         if (i < data_qps) {
             peer = qemu_get_peer(ncs, i);
         } else {
-            peer = qemu_get_peer(ncs, n->max_queues);
+            peer = qemu_get_peer(ncs, n->max_qps);
         }
         r = vhost_net_start_one(get_vhost_net(peer), dev);
 
@@ -408,7 +408,7 @@ void vhost_net_stop(VirtIODevice *dev, NetClientState *ncs,
         if (i < data_qps) {
             peer = qemu_get_peer(ncs, i);
         } else {
-            peer = qemu_get_peer(ncs, n->max_queues);
+            peer = qemu_get_peer(ncs, n->max_qps);
         }
         vhost_net_stop_one(get_vhost_net(peer), dev);
     }
